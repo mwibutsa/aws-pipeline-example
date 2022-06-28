b51f31226ecee14cccd1d930148c678b58539f6b
mwibutsa <mflohost@gmail.com>
2022-06-28 08:11:23 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "App is working" });
+});
+module.exports = app;
